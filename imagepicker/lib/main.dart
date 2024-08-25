07e6fc87a3908d0cb7125d49f6276998e355d361
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2024-08-26 02:53:31 +0600

    image_picker

--- imagepicker/lib/main.dart
+++ imagepicker/lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
@@ -15,21 +18,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  File ?imageFromGallary;
+  File? imagePicker;
   //___________________ Future Function For image _____________________
-  Future pickImageGallary() async {
+  Future pickImageGallary(ImageSource source) async {
     /*
-      ImagePicker থেকে pickImage() এ source, gallery বা camera সিলেক্ট করতে পারি ।
+      -> ImagePicker থেকে pickImage() এ source, gallery বা camera সিলেক্ট করতে পারি ।
       এমন হতে পারে যে, gallery যেতে কোন picture সিলেক্ট করলাম না । 
       সেইটা check করার জন্য নিচের condition টা । 
-      আর যদি করি । তাহলে তার path save করতে হবে । 
+
+     ->  আর যদি picture select করি । তাহলে তার path save করতে হবে । 
+
+     -> setState এই সেম function টা এই ফাইলের একাধিক জায়গায় ব্যবহার করলে । 
+
+     -> যেহেতু, flutter  তাই paltform error আসতেই পারে। 
+
     */
-    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image == null) {
-      return;
+
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) {
+        return;
+      }
+      final imageTemp = File(image.path);
+      // for use
+      setState(() {
+        imagePicker = imageTemp;
+      });
+    } on PlatformException catch (e) {
+      print("failed to pick image: $e");
     }
-    final imageTemp = File(image.path);
-    imageFromGallary = imageTemp;
   }
 
   @override
@@ -39,22 +56,42 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             const Spacer(),
+
+            // ___________ Show the image here: if the image is not null _______________
+            imagePicker != null
+                ? Image.file(
+                    imagePicker!,
+                    width: 160,
+                    height: 160,
+                    fit: BoxFit.cover,
+                  )
+                : const FlutterLogo(
+                    size: 160,
+                  ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text("Image Picker"),
             //____________ button: image from galllary ________________
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                pickImageGallary(ImageSource.gallery);
+              },
               child: const Text("Image From Gallary"),
             ),
             const SizedBox(
-              height: 20,
+              height: 50,
             ),
             //____________ button: image from camera ________________
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () async {
+                pickImageGallary(ImageSource.camera);
+              },
               icon: const Icon(
                 Icons.camera,
                 size: 20,
               ),
-              label: const Text("Image From Gallary"),
+              label: const Text("Image From camera"),
             ),
           ],
         ),
